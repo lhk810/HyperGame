34724607634424a920c2c8834c88356710e35df1
hong <lhk810@gmail.com>
2022-03-13 23:57:56 +0900

    Update logic and fix bug

--- algorithm/rule_base.py
+++ algorithm/rule_base.py
@@ -13,29 +13,34 @@ def decision_policy(dices: List[int], dices_scores: dict, current_score: domain.
     names = util.names
     if dices_scores["yacht"] > 0 and getattr(current_score, "yacht") is None:
         return {'choice' : 'yacht'}
+    if max(cnt_list) > 2 + trial and getattr(current_score, "yacht") is None:
+        return {'keep' : [idx for idx, item in enumerate(dices) if item == cnt_list.index(max(cnt_list))]}
 
-    if getattr(current_score, 'largeStraight') is None and max(util.accumulated) == 4 and trial < 3:
+    if getattr(current_score, 'largeStraight') is None and max(accumulated) == 5:
+        return {'choice' : 'largeStraight'}
+    if getattr(current_score, 'largeStraight') is None and max(accumulated) == 4 and trial < 3:
         max_val = accumulated.index(4)
         keep = []
         for i in range(max_val, max_val - 4, -1):
             keep.append(dices.index(i))
         return {'keep' : keep}
 
-    if dices_scores["fullHouse"] > 15 and getattr(current_score, 'fullHouse') is None:
+    if getattr(current_score, 'smallStraight') is None and max(util.accumulated) == 3 and trial == 0:
+        max_val = accumulated.index(3)
+        keep = []
+        for i in range(max_val, max_val-3, -1):
+            keep.append(dices.index(i))
+        return {'keep' : keep}
+
+    if dices_scores["fullHouse"] > 17 and getattr(current_score, 'fullHouse') is None:
         return {'choice' : 'fullHouse'}
 
     subtotal = 0
-    subtotal += getattr(current_score, "aces") if getattr(current_score, "aces") else 0
-    subtotal += getattr(current_score, "deuces") if getattr(current_score, "deuces") else 0
-    subtotal += getattr(current_score, "threes") if getattr(current_score, "threes") else 0
-    subtotal += getattr(current_score, "fours") if getattr(current_score, "fours") else 0
-    subtotal += getattr(current_score, "fives") if getattr(current_score, "fives") else 0
-    subtotal += getattr(current_score, "sixes") if getattr(current_score, "sixes") else 0
+    for i in range(1,7):
+        subtotal += getattr(current_score, names[i]) if getattr(current_score, names[i]) else 0
 
-    go_for_subtotal = True \
-        if None in [getattr(current_score, "aces"), getattr(current_score, "deuces"), getattr(current_score, "threes"), \
-                    getattr(current_score, "fours"), getattr(current_score, "fives"), getattr(current_score, "sixes")] \
-        else False
+    go_for_subtotal = True if None in [getattr(current_score, names[i]) for i in range(1,7)] else False
+
     if go_for_subtotal and subtotal < 63:
         max_val = 0
         max_cnt = 0
@@ -48,8 +53,8 @@ def decision_policy(dices: List[int], dices_scores: dict, current_score: domain.
         if trial < 3:
             keep = [idx for idx, item in enumerate(dices) if item == max_val]
             return {'keep' : keep}
-        if trial == 3 and max_cnt > 2:
-            if max_val > 4 and max_cnt == 3 and getattr(current_score, 'choice') is None:
+        if trial == 3 and max_cnt > 1:
+            if max_val > 4 and max_cnt < 4 and getattr(current_score, 'choice') is None:
                 return {'choice' : 'choice'}
             return {'choice' : util.names[max_val]}
 
--- algorithm/util.py
+++ algorithm/util.py
@@ -20,22 +20,20 @@ def update_accumulated(dices: List[int]):
     accumulated = [0,0,0,0,0,0,0]
     for i in range(1,7):
         accumulated[i] = accumulated[i-1] + 1 if i in dices else 0
-    print(accumulated)
 
 def score_map(dices : List[int])->dict:
     ret = {}
 
     global cnt_list
-    ret["aces"] = cnt_list[1]*1
-    ret["deuces"] = cnt_list[2]*2
-    ret["threes"] = cnt_list[3]*3
-    ret["fours"] = cnt_list[4]*4
-    ret["fives"] = cnt_list[5]*5
-    ret["sixes"] = cnt_list[6]*6
+    for i in range(1,7):
+        ret[names[i]] = cnt_list[i]*i
 
     ret["fullHouse"] = cnt_list.index(2)*2 + cnt_list.index(3)*3 if 2 in cnt_list and 3 in cnt_list else 0
     ret["fourKind"] = sum(cnt_list) if 4 in cnt_list else 0
-    ret["yacht"] = 50 if 5 in cnt_list else 0
+    ret["yacht"] = 0
+    if 5 in cnt_list:
+        ret["yacht"] = 50
+        ret["fullHouse"] = cnt_list.index(5)*5
 
     global accumulated
     ret["smallStraight"] = 15 if 4 in accumulated else 0
